d178647c6761461422ee66f411020df7ed272996
KimWooHyun <ktkwhms3@gmail.com>
2023-08-16 19:09:05 +0900

    Feat: Output Management, HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
   mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // dist 폴더를 정리 => 오래된 파일 없이 빌드에서 생성된 파일
   },
 };
