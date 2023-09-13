a36acec343908bf4bf6bff0a0b5b259ffca925cb
KimWooHyun <ktkwhms3@gmail.com>
2023-09-13 10:34:06 +0900

    Feat: code spliting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import _ from "lodash";
-import printMe from "./print.js";
+function getComponent() {
+  return import("lodash")
+    .then(({ default: _ }) => {
+      const element = document.createElement("div");
 
-function component() {
-  const element = document.createElement("div");
-  const btn = document.createElement("button");
+      element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-  return element;
+      return element;
+    })
+    .catch((error) => "An error occurred while loading the component");
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
   },
-  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // dist 폴더를 정리 => 오래된 파일 없이 빌드에서 생성된 파일
+    publicPath: "/",
+  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
@@ -15,14 +20,5 @@ module.exports = {
   ],
   devServer: {
     static: "./dist",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true, // dist 폴더를 정리 => 오래된 파일 없이 빌드에서 생성된 파일
-    publicPath: "/",
-  },
-  optimization: {
-    runtimeChunk: "single", // 단일 HTML 페이지에 하나 이상의 엔트리 포인트가 있기 때문
   },
 };
