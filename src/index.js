661424756114a9da7ae8945bb99b271b61cd3750
KimWooHyun <ktkwhms3@gmail.com>
2023-09-23 19:57:23 +0900

    Feat: PWA를 위해 웹 서버가 끊겨도 서비스워커를 통해 서비스 캐싱하여 지원

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import _ from "lodash";
-import Print from "./print";
+import printMe from "./print.js";
 
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print.bind(null, "Hello webpack!");
-
-  return element;
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
 }
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Caching",
+      title: "Progressive Web Application",
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // 이 옵션은 ServiceWorkers가 빠르게 도달하도록 장려합니다
+      // 그리고 "오래된" SW가 돌아다니는 것을 허용하지 않습니다
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
   },
 };
